c3238070359d393c1833050b268fdef5cfeb0525
Zen <lauzenjuen@gmail.com>
2021-03-11 16:31:20 +0800

    initiate committorepo

--- experiment.js
+++ experiment.js
@@ -37,6 +37,29 @@
 }
 */
 
+function commitToRepo(jsonData, path) {
+    const url = ".netlify/functions/api"
+
+    // Example:
+    const response = fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({
+            path: path,
+            data: JSON.stringify(jsonData)
+        }) // body data type must match "Content-Type" header
+    }).then((response) => {
+        console.log(response)
+    });
+}
+
 /* ----------------- Internal Functions ----------------- */
 function get_results(illusion_mean, illusion_sd, illusion_type) {
     if (typeof illusion_type != 'undefined') {
